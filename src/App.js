d9c911bde699c0598e320b2ab4c24f34502b425d
dim2a <buinicki.dim@gmail.com>
2019-11-21 16:18:32 +0300

    router setting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route, Redirect } from 'react-router';
+
+const home = () => {
+  return <div>Home</div>;
+};
+const about = () => {
+  return <div>About</div>;
+};
+const contacts = () => {
+  return <div>Contacts</div>;
+};
 
 function App() {
-  return <div>Hello!!</div>;
+  return (
+    <Switch>
+      <Route path="/about" component={about} />
+      <Route path="/contacts" component={contacts} />
+      <Route path="/" exact component={home} />
+      <Redirect to="/" />
+    </Switch>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/configureStore';
 import { Provider } from 'react-redux';
 import './index.css';
@@ -8,7 +9,9 @@ import * as serviceWorker from './serviceWorker';
 
 const app = (
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
 
