665c0ea13e4afcf72a288c833349f4ab13deffe6
dim2a <buinicki.dim@gmail.com>
2019-12-30 10:46:22 +0300

    changed classComponent to functionComponent with use react hooks

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import style from './Game.module.css';
+import classes from './Game.module.css';
 import { fetchGameById, addGameToCart } from '../../redux/actions/gamesAction';
 import { getGameById } from '../../utils/selectors';
 
-class Game extends Component {
-  componentDidMount() {
-    const { fetchGameById, match } = this.props;
-    fetchGameById(match.params.id);
-  }
+const Game = props => {
+  useEffect(() => {
+    props.fetchGameById(props.match.params.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  gameRender(game) {
-    const { addGameToCart } = this.props;
+  const gameRender = game => {
+    const { addGameToCart } = props;
     const { title, img, description, price, id } = game;
     return (
       <>
         <h2>{title}</h2>
-        <div className={style.wrapper}>
-          <img className={style.icon} src={img} alt={title} />
+        <div className={classes.wrapper}>
+          <img className={classes.icon} src={img} alt={title} />
           <p>{description}</p>
         </div>
-        <div className={style.order}>
+        <div className={classes.order}>
           <div>
             <button onClick={() => addGameToCart(id)}>Add to cart</button>
             <button onClick={() => addGameToCart(id)}>
@@ -32,13 +32,11 @@ class Game extends Component {
         </div>
       </>
     );
-  }
+  };
 
-  render() {
-    const { game } = this.props;
-    return <div>{game && this.gameRender(game)}</div>;
-  }
-}
+  const { game } = props;
+  return <div>{game && gameRender(game)}</div>;
+};
 
 const mapStateToProps = state => ({
   game: getGameById(state, state.gamePage.id),
